de27772189c5fc8be5e79d237463221e45f0bcb3
echoeureka <iherewithmyheart@gmail.com>
2022-04-20 19:03:30 +0800

    🔧 config files

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { crx } from 'rollup-plugin-chrome-extension'
+import manifest from './src/manifest'
+import zip from 'rollup-plugin-zip'
+import { resolve } from 'path'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${resolve(__dirname, 'src')}/`
+    }
+  },
+  plugins: [vue(), crx({ manifest }), isProd && zip({ dir: 'releases' })]
+})
